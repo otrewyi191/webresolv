46a0f8b80ee57283b5d79f7f58915308234a31d6
zzx <piofcircle@gmail.com>
2017-05-06 10:27:44 +0800

    origin

--- src/main/java/com/zzx/test/testReadFile.java
+++ src/main/java/com/zzx/test/testReadFile.java
@@ -1,11 +1,8 @@
 package com.zzx.test;
 
 import com.zzx.util.ReadFile;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class testReadFile {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("g:/spring.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         ReadFile readFile = (ReadFile) applicationContext.getBean(ReadFile.class);
         System.out.println("hi");
     }
